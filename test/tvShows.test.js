7432abb5509694656c43c279420640ac305ec374
Michelle Sri <michelle.sri@gmail.com>
2016-10-19 13:07:41 -0700

    initial setup with all . files and initial test of console.log is working

---
+++ test/tvShows.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const fs = require('fs');
+const tvShows = require('../lib/tvShows');
+
+describe('get my shows', () => {
+  it('gets all tv shows', (done) => {
+    console.log('tests is working');
+    done();
+  });
+});
