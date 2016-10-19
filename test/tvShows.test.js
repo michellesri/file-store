bf98b0834aa9802d102bcc234db334fcbc70fab6
Michelle Sri <michelle.sri@gmail.com>
2016-10-19 14:41:02 -0700

    successfully getting data from the directory bacl. wrote test to check if the data is expected data.

--- test/tvShows.test.js
+++ test/tvShows.test.js
@@ -1,10 +1,19 @@
-const assert = require('assert');
+const assert = require('assert'); //nodes native test, but can also use chai
 const fs = require('fs');
-const tvShows = require('../lib/tvShows');
+
+const getDirectoryData = require('../lib/tvShows');
 
 describe('get my shows', () => {
+
   it('gets all tv shows', (done) => {
-    console.log('tests is working');
-    done();
+    console.log(__dirname);
+    getDirectoryData('/' + __dirname + '/../lib/tvShowsData', (err, data) => {
+      console.log('data: ' + data);
+      assert.ok(!err);
+      //deepEqual checks for ===. equal checks for ==
+      assert.deepEqual(data, ['show1.json', 'show2.json', 'show3.json']);
+      done();
+    });
+    console.log('test is working');
   });
 });
